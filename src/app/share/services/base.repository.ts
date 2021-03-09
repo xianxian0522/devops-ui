f3187690f7da1aabfc9499f9ad60dc5f7e6ddf79
suixian.gong <suixian.gong@sumscope.com>
2021-03-09 17:46:40 +0800

    base

---
+++ src/app/share/services/base.repository.ts
@@ -0,0 +1,45 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+const AIP = '/api/v1';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BaseRepository<MODEl extends {id?: number}> {
+  constructor(protected httpClient: HttpClient) {}
+
+  queryPage(page: number, size: number, q?: {[key: string]: any}): Observable<any> {
+    const params = this.genParams(q);
+    const requestUrl = `${AIP}?size=${size}&page=${page}&${params.toString()}`;
+    return this.httpClient.get(requestUrl);
+  }
+
+  protected genParams(q?: {[key: string]: any}): URLSearchParams {
+    const params = new URLSearchParams();
+    const addValue = (key: string, value: any) => {
+      if (value === 0) {
+        value = '0';
+      }
+      if (value === false) {
+        value = 'false';
+      }
+      if (value) {
+        params.append(key, value);
+      }
+    };
+    if (q) {
+      Object.keys(q).forEach(k => {
+        const v = q[k];
+        if (v instanceof Array) {
+          v.forEach(vv => addValue(k, vv));
+          return;
+        }
+        addValue(k, v);
+      });
+    }
+    return params;
+  }
+
+}
