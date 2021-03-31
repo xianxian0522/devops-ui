193afc48ea0a580a73f972f785a99df79ac50646
suixian.gong <suixian.gong@sumscope.com>
2021-03-31 19:15:03 +0800

    cluster bind logicidcenv

--- src/app/pages/application/common-tree/common-tree.component.ts
+++ src/app/pages/application/common-tree/common-tree.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {NzTreeNodeOptions} from 'ng-zorro-antd/tree';
+import {BaseRepository} from '../../../share/services/base.repository';
+import {NzMessageService} from 'ng-zorro-antd/message';
 
 @Component({
   selector: 'app-common-tree',
@@ -8,19 +10,26 @@ import {NzTreeNodeOptions} from 'ng-zorro-antd/tree';
 })
 export class CommonTreeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private baseRepository: BaseRepository<any>,
+    private messageService: NzMessageService,
+  ) { }
 
   @Input() nodesData!: NzTreeNodeOptions[];
   @Input() clusterId!: number;
 
   ngOnInit(): void {
-    console.log(this.clusterId);
+
   }
 
   isExpandedChildren(node: NzTreeNodeOptions): void {
     node.expanded = !node.expanded;
   }
   clusterBindLogicIdcEnv(node: NzTreeNodeOptions): void {
-    console.log(node, this.clusterId);
+    const key = node.key;
+    const logicIdcEnvId = parseInt(key.split('-')[0], 10);
+    this.baseRepository.clusterBindLogicIdcEnv(this.clusterId, logicIdcEnvId).subscribe(res => {
+      this.messageService.success('集群成功绑定到逻辑机房');
+    });
   }
 }
